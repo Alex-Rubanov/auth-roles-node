98c89cce1678177ffddbbb967e307027128f7b22
Alex <rubanovv.alex@gmail.com>
2023-05-21 00:19:38 +0200

    added .env for local variables and created authRouter for our server

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
+require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
+const authRouter = require('./authRouter')
 const PORT = process.env.PORT || 5000
 
 const app = express()
 
+app.use(express.json())
+app.use('/auth', authRouter)
+
 const start = async () => {
   try {
     await mongoose.connect(
-      `mongodb+srv://alex:auth123@cluster0.urzbqxo.mongodb.net/?retryWrites=true&w=majority`
+      `mongodb+srv://alex:${process.env.DB_KEY}@cluster0.urzbqxo.mongodb.net/?retryWrites=true&w=majority`
     )
     app.listen(PORT, () => console.log(`server started on port ${PORT}`))
   } catch (error) {
